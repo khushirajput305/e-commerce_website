f98b3552129bb244cab443979eed0ac70368bfde
khushirajput305 <khushinurawat305@gmail.com>
2024-07-17 11:57:59 +0530

    make it better

---
+++ client/src/components/newnavbaar/Newnav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './newnav.css'
+
+const Newnav = () => {
+  return (
+    <div className='new_nav'>
+    <div className='nav_data'>
+        <div className='left_data'>
+            <p>All</p>
+            <p>Fresh</p>
+            <p>Amazon miniTV</p>
+            <p>Sell</p>
+            <p>Best Sellers</p>
+            <p>Mobiles</p>
+             <p>Today's Deals</p>
+           <p>Prime</p> 
+           <p>Customer Service</p>
+           <p>Electronics</p>
+           <p>New Releases</p>
+           <p>Home & Kitchen</p>
+           <p>Fashion</p>
+           <p>Gift Ideas</p>
+          <p>Amazon Pay</p>
+          <p>Computer</p>
+          <p>Book</p>
+        </div>
+    </div>        
+    </div>
+  )
+}
+
+export default Newnav
