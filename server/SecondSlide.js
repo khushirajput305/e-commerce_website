18f0b31ed463c82e335ee7616130efa6eeb402de
khushirajput305 <khushinurawat305@gmail.com>
2024-07-20 11:08:32 +0530

    make it better

---
+++ server/SecondSlide.js
@@ -0,0 +1,14 @@
+const Products2= require("./models/productsSchema2");
+const Second_slide_Data=require("./constant/productdata2");
+
+const SecondSlide=async()=>{
+    try {
+        await Products2.deleteMany({});
+        const slide2Data= await Products2.insertMany(Second_slide_Data);
+        console.log(slide2Data);
+    } catch (error) {
+        console.log("Error "+ error.message);
+    }
+}
+
+module.exports=SecondSlide;
